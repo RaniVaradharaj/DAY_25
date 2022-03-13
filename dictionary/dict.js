d741abdbe5dafad5775c5f8dd4e4694b71fe5e9e
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-03-13 17:05:05 +0530

    api

---
+++ dictionary/dict.js
@@ -0,0 +1,10 @@
+// https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+let a = document.getElementById("dict");
+let input = a.value;
+let id = document.getElementById("id");
+function search() {
+    let a = document.getElementById("dict");
+    let input = a.value;
+    console.log(input);
+    // id.innerText = input;
+}
